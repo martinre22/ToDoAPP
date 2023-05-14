2ed255af7d72f1e5a18fdddc3c148b948f2f1687
Martin Re <mlre@jerarquicos.com>
2023-05-14 12:25:00 -0300

    add and show tasks

--- app/src/main/java/com/example/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,14 +1,20 @@
 package com.example.todoapp.addtasks.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -23,7 +29,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,6 +53,41 @@ fun TasksScreen(viewModel: TasksViewModel) {
             onTaskAdded = { viewModel.onTaskCreated(it) }
         )
         FabDialog(Modifier.align(Alignment.BottomEnd), viewModel)
+        TasksList(viewModel)
+    }
+}
+
+@Composable
+private fun TasksList(viewModel: TasksViewModel) {
+    val myTasks: List<TaskModelUI> = viewModel.listTasks
+    LazyColumn {
+        items(myTasks, key = { it.idTask }) { task ->
+            ItemTask(taskModelUI = task, viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+private fun ItemTask(taskModelUI: TaskModelUI, viewModel: TasksViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        border = BorderStroke(1.dp, Color.Gray),
+        elevation = 8.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = CenterVertically) {
+            Text(
+                text = taskModelUI.task,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 4.dp)
+            )
+            Checkbox(
+                checked = taskModelUI.selected,
+                onCheckedChange = { viewModel.onCheckBoxSelected(taskModelUI) }
+            )
+        }
     }
 }
 
@@ -59,7 +103,9 @@ private fun FabDialog(pModifier: Modifier, viewModel: TasksViewModel) {
 
 @Composable
 private fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+
     var myTask by remember { mutableStateOf("") }
+
     if (show) {
         Dialog(onDismissRequest = { onDismiss() }) {
             Column(
@@ -83,7 +129,10 @@ private fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (St
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 Button(
-                    onClick = { onTaskAdded(myTask) },
+                    onClick = {
+                        onTaskAdded(myTask)
+                        myTask = ""
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(text = "Añadir tarea")
