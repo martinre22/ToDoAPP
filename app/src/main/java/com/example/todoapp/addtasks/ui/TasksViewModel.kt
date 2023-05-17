659fd5e740cb3380a9a290e681587f3a76340a49
Martin Re <mlre@jerarquicos.com>
2023-05-17 14:47:11 -0300

    implementation in presentation layer-ui

--- app/src/main/java/com/example/todoapp/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TasksViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.todoapp.addtasks.ui
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoapp.addtasks.domain.AddTaskUseCase
 import com.example.todoapp.addtasks.domain.GetTasksUseCase
-import com.example.todoapp.addtasks.ui.TaskUiState.*
+import com.example.todoapp.addtasks.ui.TaskUiState.Error
+import com.example.todoapp.addtasks.ui.TaskUiState.Loading
+import com.example.todoapp.addtasks.ui.TaskUiState.Success
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -30,15 +31,14 @@ class TasksViewModel @Inject constructor(
     private var _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
 
-    private var _listTasks = mutableStateListOf<TaskModelUI>()
-    val listTasks: List<TaskModelUI> = _listTasks
+    // private var _listTasks = mutableStateListOf<TaskModelUI>()
+    // val listTasks: List<TaskModelUI> = _listTasks
     fun onDialogClose() {
         _showDialog.value = false
     }
 
     fun onTaskCreated(task: String) {
         _showDialog.value = false
-        _listTasks.add(TaskModelUI(task = task))
         viewModelScope.launch {
             addTaskUseCase(TaskModelUI(task = task))
         }
@@ -49,14 +49,16 @@ class TasksViewModel @Inject constructor(
     }
 
     fun onCheckBoxSelected(taskModelUI: TaskModelUI) {
-        val index = _listTasks.indexOf(taskModelUI)
-        _listTasks[index] = _listTasks[index].let {
-            it.copy(selected = !it.selected)
-        }
+        // actualizar check
+//        val index = _listTasks.indexOf(taskModelUI)
+        //      _listTasks[index] = _listTasks[index].let {
+        //           it.copy(selected = !it.selected)
+        //       }
     }
 
     fun onItemRemove(taskModelUI: TaskModelUI) {
-        val task = _listTasks.find { it.idTask == taskModelUI.idTask }
-        _listTasks.remove(task)
+        // borrar item
+        //    val task = _listTasks.find { it.idTask == taskModelUI.idTask }
+        //   _listTasks.remove(task)
     }
 }
